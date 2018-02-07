e6897effa077a21b6715335412dd53dfa7f34df0
2breakegg <1216023881@qq.com>
2018-02-07 20:34:17 +0800

    ...

--- getposition.php
+++ getposition.php
@@ -1,23 +1,19 @@
 <?php
   require "conn.php";
   
-  
-  // $userid=$_POST["userid"];
-  $userid=2;
-  
-  $mysql_qry="SELECT * FROM positionlog";
+  $userid=$_POST["userid"];
+  $mysql_qry="SELECT * FROM positionlog WHERE userid=$userid";
   // var_dump
   $result = mysqli_query($conn,$mysql_qry);
 
   // $str=("lat:".(31+0.02)."lon:".(121.76+0.02)."time:12:00");
-  if($result)
-    $results = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-      $results[] = $row;
-    }
+  if($result) $results = array();
+  while ($row = mysqli_fetch_assoc($result)) {
+    $results[] = $row;
+  }
     
     // 将数组转成json格式
-    echo json_encode($results);
+  echo json_encode($results);
 
 
   // echo $str;
