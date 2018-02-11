e6f35d4303a4eacfb5d1f0fb5f8c9e7f3660dbda
2breakegg <1216023881@qq.com>
2018-02-11 18:52:37 +0800

    test0.1

--- getposition.php
+++ getposition.php
@@ -2,19 +2,24 @@
   require "conn.php";
   
   $userid=$_POST["userid"];
-  $mysql_qry="SELECT * FROM positionlog WHERE userid=$userid";
-  // var_dump
+  $start_date=$_POST["start_date"];
+  $end_date=$_POST["end_date"];
+
+  
+  // $mysql_qry="SELECT * FROM positionlog WHERE userid=$userid";
+  $mysql_qry="SELECT UNIX_TIMESTAMP(date)as date,lat,lon,userid FROM positionlog WHERE userid=$userid AND UNIX_TIMESTAMP(date) BETWEEN $start_date AND $end_date";
   $result = mysqli_query($conn,$mysql_qry);
 
-  // $str=("lat:".(31+0.02)."lon:".(121.76+0.02)."time:12:00");
-  if($result) $results = array();
+  // $str=("lat:".(31+0.02)."lon:".(121.76+0.02)."date: 12:00");
+  if($result) 
+    $results = array();
   while ($row = mysqli_fetch_assoc($result)) {
     $results[] = $row;
   }
-    
-    // 将数组转成json格式
-  echo json_encode($results);
 
+  $message=array();
+  $message['status']=1;
+  $message['data']=$results;
 
-  // echo $str;
+  echo json_encode($message);
 ?>
