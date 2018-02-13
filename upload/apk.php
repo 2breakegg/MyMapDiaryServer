6304b988a0e044bc4f9856639a7e7a80e78142f8
2breakegg <1216023881@qq.com>
2018-02-13 16:25:16 +0800

    t1

--- upload/apk.php
+++ upload/apk.php
@@ -2,6 +2,7 @@
   $apkname = $_FILES['apk']['name'];
   $tmp = $_FILES['apk']['tmp_name'];
   $filepath = $_SERVER['DOCUMENT_ROOT'].'/apks/';
+  echo $filepath.$apkname.".apk";
   if(move_uploaded_file($tmp,$filepath.$apkname.".apk")){
       echo "上传成功";
   }else{
